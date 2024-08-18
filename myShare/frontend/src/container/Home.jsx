e54208a3a07e01ba92358b844050833e999eed3f
Abhay Pratap Yadav <128727269+abhi-412@users.noreply.github.com>
2024-08-18 16:26:41 +0530

    Update Home.jsx

--- myShare/frontend/src/container/Home.jsx
+++ myShare/frontend/src/container/Home.jsx
@@ -33,7 +33,7 @@ const Home = () => {
   // console.log(user);
 
   return (
-    <div className="flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out">
+    <div className="flex bg-gray-50 md:flex-row flex-col h-[120vh] transition-height duration-75 ease-out">
       <div className="hidden md:flex h-screen flex-initial">
         <Sidebar user={user && user} />
       </div>
@@ -57,7 +57,7 @@ const Home = () => {
         )}
       </div>
      {/* {console.log(user)} */}
-      <div className="pb-2 flex-1 h-screen overflow-y-scroll" ref={scrollRef}>
+      <div className="pb-2 flex-1 h-[120vh] overflow-y-scroll hide-scrollbar" ref={scrollRef}>
         <Routes>
           <Route path="/user-profile/:userId" element={<UserProfile />} />
           <Route path="/*" element={<Pins user={user && user} />} />
